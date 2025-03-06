91c63997af24226a2a1d223fe25e184532f937e7
Md Abdus Sabur <absabur929@gmail.com>
2025-03-06 21:06:14 +0600

    cookie samesite added

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,6 +2,8 @@ exports.jwtToken = (token, res) => {
   return res.cookie("access_token", token, {
     maxAge: Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    sameSite: process.env.NODE_ENV == "production" ? "None" : "",
+    secure: process.env.NODE_ENV == "production" ? true : false,
     path: "/",
   });
 };
